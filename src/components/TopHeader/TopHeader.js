3490d543f7c8291e951ab77bb3786644bab6252b
Haniful Islam <haniful360@gmail.com>
2022-12-17 18:46:27 +0600

    header responsive

--- src/components/TopHeader/TopHeader.js
+++ src/components/TopHeader/TopHeader.js
@@ -1,29 +1,36 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import logo2 from '../../assets/logo2.png'
 import './TopHeader.css';
-import {BsGithub, BsLinkedin } from 'react-icons/bs';
+import { BsGithub, BsLinkedin } from 'react-icons/bs';
+import { FaTimes, FaBars } from 'react-icons/fa';
+
 const TopHeader = () => {
+    const navRef = useRef();
+    const showNavbar = () => {
+        navRef.current.classList.toggle('responsive_nav');
+    }
     return (
-        <div className=' to_header_container'>
-            <nav className='nav'>
+        <div className=' top_header_container'>
+            <div className='top_header'>
                 <a href="#home">
                     {/* <h2>H@niful</h2> */}
                     <img className='logo' src={logo2} alt="logo" />
                 </a>
-                <ul className='menu'>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#experience">Experience</a></li>
-                    <li><a href="#portfolio">Project</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                    <div className='social_link'>
-                        {/* <a href="https://facebook.com" target="_blank" className='social_link_icon'><BsFacebook></BsFacebook></a> */}
+                <nav ref={navRef} className='nav'>
+                    <a href="#">Home</a>
+                    <a href="#about">About</a>
+                    <a href="#experience">Experience</a>
+                    <a href="#portfolio">Project</a>
+                    <a href="#contact">Contact</a>
+                    
+                    <button onClick={showNavbar} className='nav_btn nav_close_btn'><FaTimes></FaTimes></button>
+                </nav>
+                <div className='social_link'>
                         <a href="https://linkedin.com" target="_blank" className='social_link_icon'><BsLinkedin></BsLinkedin></a>
                         <a href="https://github.com" target="_blank" className='social_link_icon'><BsGithub></BsGithub></a>
                     </div>
-                </ul>
-
-            </nav>
+                <button onClick={showNavbar} className='nav_btn'><FaBars></FaBars></button>
+            </div>
         </div>
     );
 };
